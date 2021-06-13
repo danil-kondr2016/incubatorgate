f6800f17b113f3a40e4722079627764c1f66e23a
Daniel Kondratenko <danilakondratenko95@gmail.com>
2021-06-13 20:00:58 +0600

    Внедрение HttpServer

--- src/main/java/ru/danilakondratenko/incubatorgate/Main.java
+++ src/main/java/ru/danilakondratenko/incubatorgate/Main.java
@@ -1,11 +1,20 @@
 package ru.danilakondratenko.incubatorgate;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.URI;
 import java.util.Scanner;
 import com.fazecast.jSerialComm.*;
+import com.sun.net.httpserver.*;
 
 public class Main {
     //public static Requestor requestor;
+
+    public static SerialPort port;
 
     public static SerialPort getSerialPort() {
         SerialPort[] ports = SerialPort.getCommPorts();
@@ -30,12 +39,12 @@ public class Main {
 
     public static void main(String[] args) {
 	    try {
-	        SerialPort port = getSerialPort();
+	        port = getSerialPort();
             if (port != null) {
                 port.openPort();
                 port.setComPortTimeouts(
                     SerialPort.TIMEOUT_WRITE_BLOCKING
-                  | SerialPort.TIMEOUT_READ_BLOCKING, 2000, 2000);
+                  | SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 2000, 2000);
             } else {
                 System.out.println("Error: serial port not found");
                 System.exit(0);
@@ -43,14 +52,79 @@ public class Main {
 
             //requestor = new Requestor(port);
 
-            ServerSocket serverSocket = new ServerSocket(80);
-            while (true) {
+            /*ServerSocket serverSocket = new ServerSocket(80);*/
+            /*while (true) {
                 System.out.println("Waiting socket...");
                 Server server = new Server(serverSocket.accept(), port);
                 server.start();
-            }
+            }*/
+            HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
+            server.createContext("/", new RequestHandler());
+            server.createContext("/index.html", new RequestHandler());
+            server.createContext("/control", new RequestHandler());
+            server.start();
         } catch (Exception e) {
 	        e.printStackTrace();
+        }
+    }
+
+    public static class RequestHandler implements HttpHandler {
+        public static final String DOCTYPE_HTML =
+                "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">\r\n";
+        public static final String FOOTER =
+                "<p><i>IncubatorGate Java</i></p>\r\n";
+        public static final String WELCOME_PAGE =
+                DOCTYPE_HTML +
+                        "<html>\r\n" +
+                        "<head><title>Incubator</title></head>\r\n" +
+                        "<body>\r\n" +
+                        "<h1 align=\"center\">The IoT-incubator</h1>\r\n" +
+                        "<p align=\"center\">Created by Kondratenko Daniel in 2021</p>\r\n" +
+                        "<hr>\r\n" +
+                        FOOTER +
+                        "</body>\r\n" +
+                        "</html>\r\n";
+
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            URI uri = httpExchange.getRequestURI();
+            String path = uri.getPath();
+            String method = httpExchange.getRequestMethod();
+
+            DataInputStream is = new DataInputStream(httpExchange.getRequestBody());
+            DataOutputStream os = new DataOutputStream(httpExchange.getResponseBody());
+            byte[] answerBuf = null;
+
+            if (path.compareTo("/") == 0 || path.compareTo("/index.html") == 0) {
+                answerBuf = WELCOME_PAGE.getBytes("UTF-8");
+                httpExchange.getResponseHeaders().add("Content-Type", "text/html; charset=utf-8");
+                httpExchange.sendResponseHeaders(200, answerBuf.length);
+                os.write(answerBuf);
+            } else if (path.compareTo("/control") == 0) {
+                if (method.compareTo("GET") == 0) {
+                    answerBuf = "method_get\r\n".getBytes();
+                } else if (method.compareTo("POST") == 0) {
+                    byte[] reqBuf = new byte[is.available()];
+                    is.read(reqBuf);
+
+                    port.writeBytes(reqBuf, reqBuf.length);
+                    byte[] respBuf = new byte[port.bytesAvailable()];
+                    port.readBytes(respBuf, respBuf.length);
+
+                    answerBuf = respBuf;
+                }
+                httpExchange.getResponseHeaders().add("Content-Type", "text/plain; charset=utf-8");
+                httpExchange.sendResponseHeaders(200, answerBuf.length);
+                os.write(answerBuf);
+            } else {
+                answerBuf = "Error 404.\r\n".getBytes();
+                httpExchange.sendResponseHeaders(404, answerBuf.length);
+                os.write(answerBuf);
+            }
+
+
+            os.flush();
+            os.close();
         }
     }
 }
